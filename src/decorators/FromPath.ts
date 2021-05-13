644df9f47355b30e8bed8e44fd9ca53fc5620549
Johny Herangi <johnyherangi@gmail.com>
2021-05-13 16:53:49 +1200

    use Omit<RequestOptions<T>, 'required'>

--- src/decorators/FromPath.ts
+++ src/decorators/FromPath.ts
@@ -1,9 +1,10 @@
 import { FROM_PATH_METADATA_KEY } from "../constants"
+import { RequestOptions } from "../types"
 
 /**
  * Extract a parameter from the request resource path.
  */
-export default function FromPath<T = string>(name: string, options?: { coerce: (param: string) => T }): ParameterDecorator {
+export default function FromPath<T = string>(name: string, options?: Omit<RequestOptions<T>, 'required'>): ParameterDecorator {
     return (target: any, propertyKey: string | symbol, index: number): void => {
         const existing: any[] =
             Reflect.getOwnMetadata(
