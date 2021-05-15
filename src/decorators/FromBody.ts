e700451c569fcc2b95ad134e984c59f5a29fd7b1
Johny Herangi <johnyherangi@gmail.com>
2021-05-16 11:37:10 +1200

    don't coerce request body

--- src/decorators/FromBody.ts
+++ src/decorators/FromBody.ts
@@ -1,4 +1,3 @@
-import getDefaultCoerce from "../coerce"
 import { FROM_BODY_METADATA_KEY } from "../constants"
 import { RequestOptions } from "../types"
 
@@ -6,12 +5,9 @@ import { RequestOptions } from "../types"
  * Extract and parse JSON data from the request body.
  */
 export default function FromBody(
-    options: RequestOptions = { required: true }
+    options: Omit<RequestOptions, "coerce"> = { required: true }
 ): ParameterDecorator {
     return (target: any, propertyKey: string | symbol, index: number): void => {
-        if (!options.coerce) {
-            options.coerce = getDefaultCoerce(target, propertyKey, index)
-        }
         const existing: any[] =
             Reflect.getOwnMetadata(
                 FROM_BODY_METADATA_KEY,
