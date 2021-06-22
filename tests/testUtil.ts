dbdcbc6d01cdf1be9d258cbacf13c63ff6e749b8
Johny Herangi <johny.herangi@vacasa.com>
2021-06-22 14:10:56 +1200

    Support scheduled events (#47)
    * add scheduled event handler
    
    * rename decorator, revert bad sqs change
    
    * add function docs
    
    * null check "detail-type" before use
    
    * bump minor version
    
    * update readme
    
    * bump minor version

--- tests/testUtil.ts
+++ tests/testUtil.ts
@@ -1,5 +1,10 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import { APIGatewayProxyEvent, Context, SQSEvent } from "aws-lambda"
+import {
+    APIGatewayProxyEvent,
+    Context,
+    ScheduledEvent,
+    SQSEvent
+} from "aws-lambda";
 import { APIGatewayEventFactoryArgs } from "./types";
 
 export const createAPIGatewayEvent = ({
@@ -89,6 +94,18 @@ export const createSQSEvent = (...arns: string[]): SQSEvent => ({
         eventSource: "aws:sqs",
         awsRegion: "",
     })),
+})
+
+export const createScheduledEvent = (...arns: string[]): ScheduledEvent => ({
+    version: "0",
+    account: "123456789012",
+    region: "us-east-2",
+    detail: {},
+    "detail-type": "Scheduled Event",
+    source: "aws.events",
+    time: "2019-03-01T01:23:45Z",
+    id: "cdc73f9d-aea9-11e3-9d5a-835b769c0d9c",
+    resources: arns,
 })
 
 export const createLambdaContext = (): Context => ({
