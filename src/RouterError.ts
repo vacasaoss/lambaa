56315d4461b67b1e7bbfba8b04e0298a435736a8
Jack <jackholmes5194@gmail.com>
2021-07-02 09:22:31 +1200

    fix param

--- src/RouterError.ts
+++ src/RouterError.ts
@@ -4,7 +4,7 @@ import { RouterErrorCode } from "./types"
  * Represents a failure to route an event.
  */
 export default class RouterError extends Error {
-    public isRequestParseError = true
+    public isRouterError = true
     public code: RouterErrorCode
 
     constructor({ message, code }: { message: string; code: RouterErrorCode }) {
