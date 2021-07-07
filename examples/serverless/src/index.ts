b4340690dc7c5515401e63d69c96d1c6a9035b5d
jack <jack.holmes@vacasa.com>
2021-07-07 14:39:28 +1200

    package upgrades for serverless example

--- examples/serverless/src/index.ts
+++ examples/serverless/src/index.ts
@@ -2,15 +2,14 @@ import { Router } from "lambaa"
 import PingController from "@controllers/PingController"
 import UsersController from "@controllers/UsersController"
 import QueueController from "@controllers/QueueController"
-import errorLoggerMiddleware from "./middleware/errorLoggerMiddleware"
+import errorLoggerMiddleware from "@middleware/errorLoggerMiddleware"
 
-const router = new Router({
-    controllers: [
+const router = new Router()
+    .registerControllers([
         new PingController(),
         new UsersController(),
         new QueueController(),
-    ],
-    middleware: [errorLoggerMiddleware],
-})
+    ])
+    .registerMiddleware(errorLoggerMiddleware)
 
 export const handler = router.getHandler()
