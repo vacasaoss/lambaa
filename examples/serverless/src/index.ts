937b2dbaee29ad215120abfbb2c7ba1d4494ca01
Jack Holmes <jack.holmes@vacasa.com>
2022-02-11 15:05:48 +1300

    Support apigw proxy mode for serverless offline (#69)
    * Include additional check for proxy event
    
    * update package
    
    * package upgrades for serverless example
    
    * increment version number

--- examples/serverless/src/index.ts
+++ examples/serverless/src/index.ts
@@ -2,7 +2,7 @@ import { Router } from "lambaa"
 import PingController from "@controllers/PingController"
 import UsersController from "@controllers/UsersController"
 import QueueController from "@controllers/QueueController"
-import errorLoggerMiddleware from "./middleware/errorLoggerMiddleware"
+import errorLoggerMiddleware from "@middleware/errorLoggerMiddleware"
 
 const router = new Router().registerControllers([
         new PingController(),
